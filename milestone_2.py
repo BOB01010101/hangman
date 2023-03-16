002fd9ed059e2e4d231baaa5bcda8ed0f506beaf
BOB01010101 <108944731+BOB01010101@users.noreply.github.com>
2023-03-16 15:15:55 +0000

    Create milestone_2.py

---
+++ milestone_2.py
@@ -0,0 +1,14 @@
+/*
+Create a file named milestone_2.py. This file will contain the code for the first milestone. In Python,
+Lists are used to store multiple data in a single variable. In this task, we are going to create a list of words.
+
+Step 1. Create a list containing the names of your 5 favorite fruits.
+
+Step 2. Assign this list to a variable called word_list.
+
+Step 3. Print out the newly created list to the standard output (screen).
+*/
+
+word_list = ["Strawberries", "Blackberries", "Blueberries", "Grapes", "Mango"]
+
+print(word_list)
