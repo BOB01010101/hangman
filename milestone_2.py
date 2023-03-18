d001595d8633c19d7497184baeb8089efc7dc351
BOB01010101 <108944731+BOB01010101@users.noreply.github.com>
2023-03-18 15:12:55 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,16 +1,3 @@
-/* accomplish this task, you will need to use the 'random' module. The random module is one of Python's built-in modules. It has a choice method which returns a random item from a given sequence.
-
-Step 1. Go to the first line of your file.
-
-Step 2. Write import random on the line. Note: To import a module, you have to use the import keyword at the top of the file.
-
-Step 3: Create the random.choice method and pass the word_list variable into the choice method.
-
-Step 4: Assign the randomly generated word to a variable called word.
-
-Step 5: Print out word to the standard output. Run the code several times and observe the words printed out after each run.
-*/
-
 import random 
 
 word_list = ["Strawberries", "Blackberries", "Blueberries", "Grapes", "Mango"]
