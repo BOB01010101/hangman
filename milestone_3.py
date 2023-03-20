cd4289fc385b3dcfc2f5d28b12b113f4f7f4f41e
BOB01010101 <108944731+BOB01010101@users.noreply.github.com>
2023-03-20 18:43:25 +0000

    milestone_3 update
    Created a while loop which asks the user to input a single character from the alphabet when prompted. If the input doesn't match the required criteria the user is asked again to input a valid input
    
    Then imported the random word module.
    
    Used an if block to check if a the character the user inputed is contained within a randomly generated word using the rnadom word module

--- milestone_3.py
+++ milestone_3.py
@@ -6,23 +6,13 @@ while(True):
   else:
     print("Invalid letter. Please, enter a single alphabetical character.")
 
-  if guess in secret
+from random_word import RandomWords
+
+random_words = RandomWords()
+print(random_words.get_random_word())
+
+if guess in random_words.get_random_word(): 
+  print(f'Good guess! {guess} is in the word.')
+else:
+  print(f"Sorry, {guess} is not in the word. Try again.")
   
-  
-
-
-  
- /* Check whether the letter guessed by the user is in the secret word
-  that was randomly chosen by the computer. For example, if the user guesses 
-  the letter "a" and the secret word is "apple", then your code should check if "a" is in "apple".
-
-Step 1. Create an if statement that checks if the guess is in the word.
-
-Step 2. In the body of the if statement, print a message saying "Good guess! {guess} is in the word.". Obviously, format the string to show the actual guess instead of {guess}.
-
-Step 3. Create an else block that prints a message saying "Sorry, {guess} is not in the word. Try again." This block of code will run if the guess is not in the word.
-
-Getting stuck on this task? Click here t
-
-*/
-
