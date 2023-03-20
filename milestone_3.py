0c4bad23b636f7bf7de87dcfe952dd9baf97a56d
BOB01010101 <108944731+BOB01010101@users.noreply.github.com>
2023-03-20 18:06:57 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -6,4 +6,23 @@ while(True):
   else:
     print("Invalid letter. Please, enter a single alphabetical character.")
 
+  if guess in secret
   
+  
+
+
+  
+ /* Check whether the letter guessed by the user is in the secret word
+  that was randomly chosen by the computer. For example, if the user guesses 
+  the letter "a" and the secret word is "apple", then your code should check if "a" is in "apple".
+
+Step 1. Create an if statement that checks if the guess is in the word.
+
+Step 2. In the body of the if statement, print a message saying "Good guess! {guess} is in the word.". Obviously, format the string to show the actual guess instead of {guess}.
+
+Step 3. Create an else block that prints a message saying "Sorry, {guess} is not in the word. Try again." This block of code will run if the guess is not in the word.
+
+Getting stuck on this task? Click here t
+
+*/
+
